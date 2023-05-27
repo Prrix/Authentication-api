94324b5f8cc90e0684b316934d48c15b4defdfe9
Prrix <pgupta0049@gmail.com>
2023-05-27 19:10:26 +0530

    Notes API completed with port change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 var bodyParser = require('body-parser')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const userRouter =  require("./routes/userRoutes");
 const { default: mongoose } = require('mongoose');
 const { noteRouter } = require('./routes/noteRoutes');
